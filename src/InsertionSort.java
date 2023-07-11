d17afef58f9ee12a45edf3907149f7b7868f526e
RindraIBI <rindraibi@gmail.com>
2023-07-11 21:44:23 +0300

    Insertion Sorting algorithm preparation

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,6 +1,18 @@
-
+/**
+ * 
+ * @author Rindra Mbolamananamalala
+ *
+ */
 public class InsertionSort {
-	public static void main(String []args) {
-		System.out.println("Hello World!");
+	/**
+	 * 
+	 * Sort all the elements contained within an input array with the Insertion Sort algorithm 
+	 * @param unsortedArray The array the elements of which are to be sorted
+	 * @return A sorted version of the unsorted array provided as a parameter
+	 */
+	public static int []sort(int []unsortedArray) {
+		
+		// For the now
+		return null;
 	}
 }
