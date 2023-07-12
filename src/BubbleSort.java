e6ef3e79b987fddab7ef63e3545949437f955ea8
RindraIBI <rindraibi@gmail.com>
2023-07-12 21:24:55 +0300

    Preparation of the Bubble Sort algorithm's implementation

---
+++ src/BubbleSort.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Rindra Mbolamananamalala
+ * a.k.a : "Sinking Sort" algorithm 
+ */
+
+public class BubbleSort {
+	public static int []sort(int []unsortedArray) {
+		// Preparations of the result to return (the sorted array)
+		int []result = unsortedArray;
+		
+		return result;
+	}
+}
