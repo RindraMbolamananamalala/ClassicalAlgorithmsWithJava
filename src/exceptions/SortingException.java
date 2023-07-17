4407a22dbc8a2edc5514602da5132cb60d76a6c2
RindraIBI <rindraibi@gmail.com>
2023-07-17 16:32:03 +0300

    Adding an Exception management layer to the architecture

---
+++ src/exceptions/SortingException.java
@@ -0,0 +1,30 @@
+package exceptions;
+
+/**
+ * 
+ * @author Rindra Mbolamananamalala
+ * The class dedicated to the implementation of the Exception, specific to the Sorting Algorithm proposed
+ * within this Project.
+ */
+public class SortingException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1784365450662151679L;
+	
+	/**
+	 * 
+	 * @param array The array concerned by the Exception
+	 * @param exceptionMessage The message to display as an additional information 
+	 */
+	public SortingException(int []array, String exceptionMessage){
+		super(
+			"Cannot continue with the Sorting process of the Array"
+				+ "\"" 
+				+ array.toString()
+				+ "\"" + ".\n"
+				+ exceptionMessage
+		);
+	}
+}
